fda843555b2522243265bc43732af9b40f2e320d
Harley Noriega <harleyn60@gmail.com>
2021-06-09 10:09:56 -0600

    Agregue Ejercicio 5 Java

---
+++ Ejercicio5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String []args){
+    Scanner scanner = new Scanner (System.in);
+    int num;
+    int numSig;
+    System.out.println("Ingrese un numero");
+    num=scanner.nextInt();
+    numSig=num+1;
+    System.out.println("El numero siguiente es "+numSig);
+    
+}
+}
